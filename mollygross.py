6e2f0779d025b06b36de3d6027b51b10058e2102
Maureen-Maina <146948714+Maureen-Maina@users.noreply.github.com>
2024-02-04 19:06:49 +0300

    Gross salary

---
+++ mollygross.py
@@ -0,0 +1,10 @@
+#calculate gross pay
+
+hours_worked = float(input("Enter the number of hours worked: "))
+rate_per_hour = float(input("Enter the rate per hour: "))
+
+# Calculate gross pay
+gross_pay = hours_worked * rate_per_hour
+
+# Display the result
+print(f"The gross pay is: {gross_pay}")
